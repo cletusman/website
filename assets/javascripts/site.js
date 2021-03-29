2afd89a22a2180166aaff66319bddb8631a5fe7d
Alex Kotov <kotovalexarian@gmail.com>
2021-03-29 13:52:40 +0500

    Use Play video player

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,3 +1,8 @@
-import 'jquery'
+import jQuery from 'jquery'
 import 'popper.js'
 import 'bootstrap/dist/js/bootstrap'
+import Plyr from 'plyr'
+
+jQuery(function() {
+  const plyr = new Plyr('#video-main')
+})
